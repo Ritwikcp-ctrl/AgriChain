c04e12a708ddd2b0d99211c27f5a849e0e9f282e
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-07-29 16:52:36 +0530

    model : category-model.ts

---
+++ apps/api/src/models/category-model.ts
@@ -0,0 +1,43 @@
+import mongoose ,{Schema} from "mongoose";
+
+
+const categorySchema = new Schema({
+    name:{
+        type:String,
+        required :true,
+        unique:true,
+        trim:true,
+    },
+
+    slug:{
+        type:String,
+        required:true,
+        unique : true,
+        lowercase : true,
+    },
+
+    description : {
+        type : String,
+
+    },
+
+    icon : {
+        type : String,
+
+    },
+
+    image : {
+        type : String,
+    },
+
+    price :{
+       type:Number,
+       required : true,
+       min:0,
+    },
+
+    isActive : {
+        type : Boolean,
+        default : false,
+    }
+}, {timestamps:true})
