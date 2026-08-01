88b3c66626e28128cf1f168591626f85c9d35dd7
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-01 14:23:34 +0530

    added : router

--- apps/api/src/routes/user-router.ts
+++ apps/api/src/routes/user-router.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 import { registerUser } from "../controllers/user-registerControllers";
 import requestValidation from "../middlewares/Uservalidation";
 import { userRegisterSchema } from "../utils/regiaterUserSchema";
-
+import { loginUserSchema } from "../utils/loinUserSchema";
+import { loginUser } from "../controllers/user-loginControllers";
 
 const router = Router();
 
 router
-  .route("/register")
-  .post(requestValidation(userRegisterSchema), registerUser );
+    .route("/register")
+    .post(requestValidation(userRegisterSchema), registerUser);
+
+router
+    .route("/login")
+    .post(requestValidation(loginUserSchema),
+    loginUser);
 
 export default router;
