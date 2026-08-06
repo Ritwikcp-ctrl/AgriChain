307a2eb9147e0fcf45e117fdd7f032b137d1c22a
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-06 11:18:27 +0530

    added : router for the category

--- apps/api/src/routes/user-router.ts
+++ apps/api/src/routes/user-router.ts
@@ -12,9 +12,11 @@ import requestValidation from "../middlewares/Uservalidation";
 import { userRegisterSchema } from "../utils/regiaterUserSchema";
 import { loginUserSchema } from "../utils/loinUserSchema";
 import { verifyJwt } from "../middlewares/verifyToken";
+import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCatogory } from "../controllers/category-controller";
 
 const router = Router();
 
+//user routers
 router
   .route("/register")
   .post(requestValidation(userRegisterSchema), registerUser);
@@ -30,5 +32,16 @@ router.route("/current-user").post(verifyJwt, getCurrentUser);
 router.route("/change-password").post(verifyJwt, changeCurrentPassword);
 
 router.route("/update-account").post(verifyJwt, updateAccountDetails);
+
+//category routers
+router.route("/:createCat").post(verifyJwt,createCategory);
+
+router.route("/:getAllcat").get(getAllCategories);
+
+router.route("/:getCatId").get(getCategoryById);
+
+router.route("/:updateCat").patch(verifyJwt,updateCatogory);
+
+router.route("/:deleteCat").delete(verifyJwt,deleteCategory);
 
 export default router;
