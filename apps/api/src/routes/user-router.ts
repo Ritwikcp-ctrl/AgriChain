2f90d7f0ee4c784452ba74e018b0719d1e26beb8
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-06 19:07:59 +0530

    added router for product

--- apps/api/src/routes/user-router.ts
+++ apps/api/src/routes/user-router.ts
@@ -12,7 +12,14 @@ import requestValidation from "../middlewares/Uservalidation";
 import { userRegisterSchema } from "../utils/regiaterUserSchema";
 import { loginUserSchema } from "../utils/loinUserSchema";
 import { verifyJwt } from "../middlewares/verifyToken";
-import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCatogory } from "../controllers/category-controller";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCatogory,
+} from "../controllers/category-controller";
+import { createProduct } from "../controllers/product-controller";
 
 const router = Router();
 
@@ -34,14 +41,16 @@ router.route("/change-password").post(verifyJwt, changeCurrentPassword);
 router.route("/update-account").post(verifyJwt, updateAccountDetails);
 
 //category routers
-router.route("/:createCat").post(verifyJwt,createCategory);
+router.route("/:createCat").post(verifyJwt, createCategory);
 
 router.route("/:getAllcat").get(getAllCategories);
 
 router.route("/:getCatId").get(getCategoryById);
 
-router.route("/:updateCat").patch(verifyJwt,updateCatogory);
+router.route("/:updateCat").patch(verifyJwt, updateCatogory);
 
-router.route("/:deleteCat").delete(verifyJwt,deleteCategory);
+router.route("/:deleteCat").delete(verifyJwt, deleteCategory);
+
+router.route("/:createProd").post(verifyJwt, createProduct);
 
 export default router;
