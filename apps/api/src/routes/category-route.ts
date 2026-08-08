3ccc8843275d9b450e98e1fbbcd0f5772714bbf5
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-08 10:54:46 +0530

    added : route for category

---
+++ apps/api/src/routes/category-route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {
+  createCategory,
+  deleteCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCatogory,
+} from "../controllers/category-controller";
+import { verifyJwt } from "../middlewares/verifyToken";
+
+const router = Router();
+
+
+
+router.route("/:createCat").post(verifyJwt, createCategory);
+
+router.route("/:getAllcat").get(getAllCategories);
+
+router.route("/:getCatId").get(getCategoryById);
+
+router.route("/:updateCat").patch(verifyJwt, updateCatogory);
+
+router.route("/:deleteCat").delete(verifyJwt, deleteCategory);
+
+export default router;
