21438398523765bb73997b338197fd7c26393564
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-10 20:33:49 +0530

    added :few rental logic

--- apps/api/src/routes/rental-routes.ts
+++ apps/api/src/routes/rental-routes.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/verifyToken";
 import {
   acceptRental,
+  cancelRental,
+  completeRental,
   createRentalRequest,
   getAllRentalById,
   getAllRentals,
@@ -20,5 +22,9 @@ router.route("/getbyId").get(verifyJwt, getAllRentalById);
 router.route("/acceptrent").patch(verifyJwt, acceptRental);
 
 router.route("/:rentalId/reject").patch(verifyJwt, rejectRental);
+
+router.route("/:rentalId/complete").patch(verifyJwt, completeRental);
+
+router.route("/:rentalId/cancel").patch(verifyJwt, cancelRental);
 
 export default router;
