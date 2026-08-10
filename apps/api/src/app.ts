61d479bc1475b44b7ba077bb2ba2364283c2bbac
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-10 20:31:14 +0530

    added :anoter routes for order

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,21 +2,27 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
-}));
+  })
+);
 
-app.use(express.json({
-    limit : "23kb"
-}));
+app.use(
+  express.json({
+    limit: "23kb",
+  })
+);
 
-app.use(express.urlencoded({
-    extended : true,limit : "23kb"
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "23kb",
+  })
+);
 
 app.use(express.static("public"));
 app.use(cookieParser());
@@ -26,12 +32,13 @@ import categoryRouter from "./routes/category-route";
 import productsRouter from "./routes/products-route";
 import toolsRouter from "./routes/tool-routes";
 import rentalRouter from "./routes/rental-routes";
-
+import orderRouter from "./routes/order-routes";
 
 app.use("api/v1/users", userRouter);
-app.use("api/v1/category",categoryRouter);
-app.use("api/v1/products",productsRouter);
-app.use("api/v1/tools",toolsRouter);
-app.use("api/v1/rental",rentalRouter)
+app.use("api/v1/category", categoryRouter);
+app.use("api/v1/products", productsRouter);
+app.use("api/v1/tools", toolsRouter);
+app.use("api/v1/rental", rentalRouter);
+app.use("api/v1/order", orderRouter);
 
 export default app;
