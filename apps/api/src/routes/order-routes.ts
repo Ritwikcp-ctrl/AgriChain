b950b0ada24251231c7c8153fe53742a33c7527f
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-11 20:10:10 +0530

    added : order routes

--- apps/api/src/routes/order-routes.ts
+++ apps/api/src/routes/order-routes.ts
@@ -3,10 +3,22 @@ import { Router } from "express";
 const router = Router();
 
 import { verifyJwt } from "../middlewares/verifyToken";
-import { createOrder, getOrders } from "../controllers/order-controller";
+import {
+  cancelOrder,
+  createOrder,
+  getOrderById,
+  getOrders,
+  updateOrderStatus,
+} from "../controllers/order-controller";
 
 router.route("/createorder").post(verifyJwt, createOrder);
 
 router.route("/getorder").get(verifyJwt, getOrders);
+
+router.route("/getbyId").get(verifyJwt, getOrderById);
+
+router.route("/:orderid/cancel").patch(verifyJwt, cancelOrder);
+
+router.route("/:orederUpdate").patch(verifyJwt, updateOrderStatus);
 
 export default router;
