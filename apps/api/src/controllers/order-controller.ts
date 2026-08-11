e71f5a9bd85e031513bc49236467bf09863e8ca6
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-11 20:09:44 +0530

    added : order logic

--- apps/api/src/controllers/order-controller.ts
+++ apps/api/src/controllers/order-controller.ts
@@ -74,3 +74,103 @@ export const getOrders = asyncHandler(
       .json(new ApiResponse(200, orders, "orders fetched successfully"));
   }
 );
+
+export const getOrderById = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId)
+      .populate("buyer", "fullname email")
+      .populate("seller", "fullname email")
+      .populate("product");
+
+    if (!order) {
+      throw new ApiError(404, "order not found");
+    }
+    //only buyer or seller can view this order
+    const userId = req.user?._id.toString();
+
+    const isBuyer = order.buyer._id.toString() === userId;
+    const isSeller = order.seller._id.toString() === userId;
+
+    if (!isBuyer && !isSeller) {
+      throw new ApiError(404, "you are not allowed to view this order");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, order, "order fetched successfully"));
+  }
+);
+
+export const cancelOrder = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new ApiError(404, "order not found");
+    }
+
+    if (order.buyer.toString() !== req.user?._id.toString()) {
+      throw new ApiError(404, "only the buyer can cancel this order");
+    }
+
+    if (order.status !== "pending") {
+      throw new ApiError(
+        400,
+        `order cannot be cancelled because its status is ${order.status}`
+      );
+    }
+
+    const product = await Products.findById(order.product);
+    if (!product) {
+      throw new ApiError(404, "product not found");
+    }
+    product.quantity += order.quantity;
+    product.availability = true;
+    await product.save();
+
+    order.status = "cancelled";
+    await order.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, order, "order cancelled successfully"));
+  }
+);
+
+export const updateOrderStatus = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const allowedStatus = ["confirmed", "shipped", "delivered"];
+
+    if (!status || !allowedStatus.includes(status)) {
+      throw new ApiError(404, "Invalid order status");
+    }
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new ApiError(404, "order not found");
+    }
+
+    if (order.seller.toString() !== req.user?._id.toString()) {
+      throw new ApiError(404, "only the seller can update the order status");
+    }
+    if (status === "confirmed" && order.status !== "pending") {
+      throw new ApiError(404, "only the pending orders can be confirmed");
+    }
+    if (status === "shipped" && order.status !== "confirmed") {
+      throw new ApiError(404, "only confirmed orders can be shipped");
+    }
+    if (status === "deliverd" && order.status !== "shipped") {
+      throw new ApiError(404, "only shipped orders can be delivered");
+    }
+
+    order.status = status;
+    await order.save();
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200,order, `order ${status} successfully`));
+  }
+);
