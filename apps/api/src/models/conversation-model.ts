305a9f59b354af3b23e243b9f410b3ae47a541c6
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-15 14:26:33 +0530

    added : new model for conversation

---
+++ apps/api/src/models/conversation-model.ts
@@ -0,0 +1,44 @@
+import mongoose ,{Document,mongo,Schema,Types} from "mongoose";
+
+export interface IConversation extends Document {
+    participants :Types.ObjectId[];
+    cropId:Types.ObjectId;
+    transactionId?:Types.ObjectId;
+    status : "active" | "closed";
+    createedAt:Date;
+    updatedAt:Date;
+}
+
+const conversationSchema = new Schema<IConversation> ({
+    participants :[
+        {
+            type : Schema.Types.ObjectId,
+            ref:"User",
+            required : true,
+        },
+    ],
+
+    cropId: {
+        type : Schema.Types.ObjectId,
+        ref:"Crop",
+        required :true,
+    },
+
+    transactionId : {
+        type:Schema.Types.ObjectId,
+        ref : "Transection",
+        required : true,
+    },
+
+    
+    status : {
+        type : String,
+        enum : ["active","closed"],
+        default : "active",
+
+    },
+    
+},{timestamps:true,})
+
+
+export const Conversation = mongoose.model<IConversation> ("Conversation",conversationSchema);
