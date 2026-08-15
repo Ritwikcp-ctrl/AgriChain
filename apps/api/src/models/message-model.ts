a372d44101f307604bcf3a7a6272356ef9d3c16d
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-15 23:57:31 +0530

    added : message model

---
+++ apps/api/src/models/message-model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface IMessage extends Document {
+  conversationId: Types.ObjectId;
+  senderId: Types.ObjectId;
+  message: string;
+  createdAt: Date;
+  updateAt: Date;
+}
+
+const messageSchema = new Schema<IMessage>(
+  {
+    conversationId: {
+      type: Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+
+    senderId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    message: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model<IMessage>("Messagfe", messageSchema);
