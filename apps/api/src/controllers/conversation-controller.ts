9fab69f4b45a39fede3191d73e381e008884a431
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-16 17:03:00 +0530

    added :conversation controller

---
+++ apps/api/src/controllers/conversation-controller.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import { Conversation } from "../models/conversation-model";
+import { Transection } from "../models/trasection-model";
+import { asyncHandler } from "../utils/asyncHandler";
+import ApiError from "../utils/ApiError";
+import ApiResponse from "../utils/ApiResponse";
+
+export const createConversation = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { transactionId } = req.body;
+
+    if (!transactionId) {
+      return new ApiError(404, "invalid transactionId");
+    }
+    const transaction = await Transection.findById(transactionId);
+    if (!transaction) {
+      return new ApiError(404, "transaction not found");
+    }
+
+    const existingConversation = await Conversation.findOne({
+      transactionId: transaction._id,
+    });
+
+    if (existingConversation) {
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            existingConversation,
+            "conversation already exists"
+          )
+        );
+    }
+
+    const consversation = await Conversation.create({
+        participants : [transaction.farmerId, transaction.buyerId],
+        cropId: transaction.cropId,
+        transactionId:transaction._id,
+        status : "active",
+    })
+
+    return res.status(201).json(new ApiResponse(200,consversation,"conversation created successfully")
+
+    )
+  }
+);
