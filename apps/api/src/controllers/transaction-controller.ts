51a0c975b8a3e8a9bf57ed5af5bb769b82121407
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-08-25 12:42:37 +0530

    added : transaction logic

---
+++ apps/api/src/controllers/transaction-controller.ts
@@ -0,0 +1,114 @@
+import { NextFunction, Request, Response } from "express";
+
+import { Conversation } from "../models/conversation-model";
+import { Transaction } from "../models/trasection-model";
+import { CropBroadcast } from "../models/cropBroadcast.model";
+
+import { asyncHandler } from "../utils/asyncHandler";
+import ApiError from "../utils/ApiError";
+import ApiResponse from "../utils/ApiResponse";
+
+
+export const createTransaction = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { conversationId, quantity, pricePerUnit } = req.body;
+
+    if (!req.user) {
+      throw new ApiError(401, "Unauthorized");
+    }
+
+    if (!conversationId) {
+      throw new ApiError(400, "conversation ID is required");
+    }
+
+    if (quantity === undefined || quantity <= 0) {
+      throw new ApiError(401, "Quantity must be greater than 0");
+    }
+
+    if (pricePerUnit === undefined || pricePerUnit < 0) {
+      throw new ApiError(400, "Invalid price per unit");
+    }
+
+    const conversation = await Conversation.findById(conversationId);
+    if (!conversation) {
+      throw new ApiError(404, "Conversation not found");
+    }
+
+    if (conversation.status !== "active") {
+      throw new ApiError(400, "Conversation is closed");
+    }
+
+    const userId = req.user._id.toStrig();
+    const isParticipant = conversation.participants.some(
+      (participant) => participant.toString() === userId
+    );
+
+    if (!isParticipant) {
+      throw new ApiError(
+        403,
+        "Transaction already exists for the conversation"
+      );
+    }
+
+    if (conversation.transactionId) {
+      throw new ApiError(
+        409,
+        "Transaction already exist for this conversation"
+      );
+    }
+
+    const broadcast = await CropBroadcast.findById(conversation.broadcastId);
+
+    if (!broadcast) {
+      throw new ApiError(404, "Broadcast not found");
+    }
+
+    const farmerId = broadcast.farmer;
+
+    const buyerId = conversation.participants.find(
+      (participant) => participant.toString() !== farmerId.toString()
+    );
+
+    if (!buyerId) {
+      throw new ApiError(400, "Buyer could not be determined");
+    }
+
+    const isFarmer = farmerId.toString() === userId;
+
+    const isBuyer = buyerId.toString() === userId;
+
+    if (!isFarmer && !isBuyer) {
+      throw new ApiError(
+        403,
+        "You are not authorized to create this transaction"
+      );
+    }
+
+    if (quantity > broadcast.quantity) {
+      throw new ApiError(404, `Only ${broadcast.quantity} units are available`);
+    }
+
+    const totalAmount = quantity * pricePerUnit;
+
+    const transaction = await Transaction.create({
+      buyerId,
+      farmerId,
+      cropId: conversation.cropId,
+      quantity,
+      pricePerUnit,
+      totalAmount,
+      status: "pending",
+    });
+
+    conversation.transactionId = transaction._id;
+
+    await conversation.save();
+
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, transaction, "Transaction created succefully")
+      );
+  }
+);
+
